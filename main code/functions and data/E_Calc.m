b5c798d17a071081c301ea1b07ecdde2b28abc83
Ahmed Magdy <ahmagdy1080@gmail.com>
2025-12-15 10:28:08 +0200

    will put them back online after making changes

--- main code/functions and data/E_Calc.m
+++
@@ -1,5 +0,0 @@
-function out = E_Calc(E, t, e, ti, T)
-% Solves Kepler's Equation for the Eccentric Anomaly (E):
-% $f(E) = t + t_i - \frac{T}{2\pi} (E - e \sin E) = 0$
- out = t + ti - T / (2 * pi) * (E - e * sin(E));
-end
--- main code/functions and data/sunTerminator.m
+++
@@ -1,9 +0,0 @@
-function [lonTerm, latTerm] = sunTerminator(subsolar_lon, subsolar_lat)
-
-    lonTerm = 180:-1:-180;
-    H = deg2rad(lonTerm - subsolar_lon);      % hour angle
-    dec = deg2rad(subsolar_lat);
-
-    % Terminator latitude formula:
-    latTerm = rad2deg( atan( -cos(H) ./ tan(dec) ) );
-end
